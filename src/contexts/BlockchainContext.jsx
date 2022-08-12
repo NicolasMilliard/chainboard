6e46390f7dc83d47f642ca26802d97fc33ff6691
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-12 15:45:53 +0200

    Init commit

---
+++ src/contexts/BlockchainContext.jsx
@@ -0,0 +1,74 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import contractConfig from '../contract-config.json'
+import { ethers } from 'ethers'
+
+const { abi, contractAddress } = contractConfig;
+
+const BlockchainContext = createContext();
+
+export const BlockchainProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    // Read-only access to the blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+    // Sign messages and transactions on the blockchain
+    const signer = provider.getSigner();
+
+    // Contract (BSC Testnet)
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    // Connect wallet and store address in the state
+    const connectWallet = async () => {
+        try {
+            // Is Metamask installed?
+            if(!window.ethereum) return alert("Please install Metamask.")
+
+            // Prompt user for account connection
+            const accounts = await provider.send("eth_requestAccounts", []);
+            // Save user address in the state
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
+        }
+    }
+
+    // Check if wallet is connected
+    const checkWalletConnection = async () => {
+        try {
+            // Is Metamask installed?
+            if(!window.ethereum) return alert("Please install Metamask.")
+
+            // Request the use account
+            const accounts = await provider.send("eth_accounts", []);
+
+            if(accounts.length) {
+                // Save user address in the state
+                setCurrentAccount(accounts[0]);
+            } else {
+                console.log("No accounts found");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // When variables changes
+    useEffect(() => {
+        checkWalletConnection();
+    }, [])
+
+  return (
+    <BlockchainContext.Provider
+        value={{
+            connectWallet,
+            currentAccount
+        }}
+    >
+        {children}
+    </BlockchainContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(BlockchainContext);
