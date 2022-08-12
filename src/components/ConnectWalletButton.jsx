59b1bd528f4e7bee12aa5cdc053dd358b07027c3
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-12 15:45:46 +0200

    Add connectWallet function

--- src/components/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useStateContext } from '../contexts/BlockchainContext'
 
 const ConnectWalletButton = () => {
+  const { connectWallet, currentAccount } = useStateContext();
+
   return (
-    <button type='button' className='bg-[#f2504b] px-4 py-2 text-white text-14 sm:text-base font-semibold rounded-3xl hover:drop-shadow-lg'>Connect wallet</button>
+    <button
+      type='button'
+      className='bg-[#f2504b] px-4 py-2 text-white text-14 sm:text-base font-semibold rounded-3xl hover:drop-shadow-lg'
+      onClick={connectWallet}
+    >
+      { !currentAccount ? "Connect wallet" : `${currentAccount.slice(0, 5)}...${currentAccount.slice(currentAccount.length - 4)}` }
+    </button>
   )
 }
 
