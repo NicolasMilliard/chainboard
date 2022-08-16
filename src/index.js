8866d64ea729b7556a3eabdd3c11f8b6723f0aa0
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-16 16:42:00 +0200

    Add ContextProvider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './index.css'
 import App from './App'
 
 import { BlockchainProvider } from './contexts/BlockchainContext'
+import { ContextProvider } from './contexts/ContextProvider'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
     <BlockchainProvider>
-        <App />
+        <ContextProvider>
+            <App />
+        </ContextProvider>
     </BlockchainProvider>
 );
