2ea46f32cbb3d7db6856f1ff612262b5cd72e982
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-16 16:41:48 +0200

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Rent from './pages/Rent'
+
+import { useStateBlockchainContext } from './contexts/BlockchainContext'
 
 import './App.css'
-import Navbar from './components/Navbar'
-import ConnectWalletButton from './components/ConnectWalletButton'
-
-import bsc from './img/logo/bsc.svg'
 
 const App = () => {
+    const { connectWallet, currentAccount } = useStateBlockchainContext();
   return (
     <>
         <Navbar />
-        {/* Hero section height = 100vh - ([NavBar height] + main content margin top)  */}
-        <div className='chainboard-hero-bg h-[calc(100vh_-_10rem)] sm:h-[calc(100vh_-_15rem)] bg-no-repeat bg-right-bottom'>
-            <div className='flex flex-col justify-between h-full max-w-7xl mx-auto mt-20 sm:mt-40 px-8'>
-                <div>
-                    <h1 className='font-extrabold text-2xl sm:text-6xl sm:leading-tight'>Rediscover the world<br />thanks to <span className='text-[#f2504b]'>Blockchain</span></h1>
-                    <p className='mt-10 max-w-md leading-7'>Rent your snowboard at the real cost thanks to the blockchain technology.</p>
-                    <p className='mb-10 max-w-md leading-7'>Connect your wallet, choose your snowboard and you're ready to go!</p>
-                    <ConnectWalletButton />
-                </div>
-                <div className='mt-10 mb-8'>
-                    <p className='chainboard-bsc-text uppercase text-[#858585] font-medium '>Powered by</p>
-                    <img src={bsc} alt='Binance Smart Chain' className='chainboard-bsc-logo' />
-                </div>
-            </div>
-        </div>
+        {/* Display snowboards selection if connected. If not, display homepage. */}
+        {currentAccount ? <Rent /> : <Home />}
     </>
   )
 }
