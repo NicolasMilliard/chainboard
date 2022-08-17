c26efc62d6bd87df5f2cf736094a345fd1665b11
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-17 12:01:08 +0200

    Rename button classes

--- src/components/LevelSelect.jsx
+++ src/components/LevelSelect.jsx
@@ -6,17 +6,17 @@ const LevelSelect = () => {
     <div className='mb-20'>
         <h2 className='font-semibold text-xl sm:text-2xl sm:leading-tight mb-8'>Select your level:</h2>
         {/* Beginner */}
-        <button type='button' className='chainboard-icon-button flex items-center' onClick={() => {}}>
+        <button type='button' className='chainboard-icon-flash-button-active flex items-center' onClick={() => {}}>
             <Flash />
             <span className='ml-2'>Beginner</span>
         </button>
         {/* Intermediate */}
-        <button type='button' className='chainboard-icon-button flex items-center' onClick={() => {}}>
+        <button type='button' className='chainboard-icon-flash-button flex items-center' onClick={() => {}}>
             <Flash /><Flash />
             <span className='ml-2'>Intermediate</span>
         </button>
         {/* Expert */}
-        <button type='button' className='chainboard-icon-button flex items-center' onClick={() => {}}>
+        <button type='button' className='chainboard-icon-flash-button flex items-center' onClick={() => {}}>
             <Flash /><Flash /><Flash />
             <span className='ml-2'>Expert</span>
         </button>
