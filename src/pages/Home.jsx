9542408a3db962f603a32a73a61b7490b7ab24cc
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-17 12:01:55 +0200

    Update class names for BG Hero animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ import bsc from '../img/logo/bsc.svg'
 const Home = () => {
   return (
     /* Hero section height = 100vh - ([NavBar height] + main content margin top)  */    
-    <div className='chainboard-hero-bg h-[calc(100vh_-_10rem)] sm:h-[calc(100vh_-_15rem)] bg-no-repeat bg-right-bottom'>
-            <div className='flex flex-col justify-between h-full max-w-7xl mx-auto mt-20 sm:mt-40 px-8'>
+    <div className='chainboard-hero-bg h-[calc(100vh_-_10rem)] sm:h-[calc(100vh_-_15rem)]'>
+            <div id='hero-text-container' className='flex flex-col justify-between h-full max-w-7xl mx-auto mt-20 sm:mt-40 px-8'>
                 <div>
                     <h1 className='font-extrabold text-2xl sm:text-6xl sm:leading-tight'>Rediscover the world<br />thanks to <span className='text-[#f2504b]'>Blockchain</span></h1>
                     <p className='mt-10 max-w-md leading-7'>Rent your snowboard at the real cost thanks to the blockchain technology.</p>
