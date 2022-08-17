b35ed1e07c28d11611daa136a204bb2536482552
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-17 12:02:09 +0200

    Add ProductSlider component

--- src/pages/Rent.jsx
+++ src/pages/Rent.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 import LevelSelect from '../components/LevelSelect'
 import SnowboardSize from '../components/SnowboardSize'
+import ProductSlider from '../components/ProductSlider'
 
 const Rent = () => {
   return (
-    <div className='flex flex-col justify-between h-full max-w-7xl mx-auto mt-20 sm:mt-40 px-8'>
-        <div>
+    <div className='flex flex-wrap h-full max-w-7xl mx-auto mt-20 sm:mt-40 px-8'>
+        <div className='flex flex-col justify-between'>
             <LevelSelect />
             <SnowboardSize />
+        </div>
+        <div>
+          <ProductSlider />
         </div>
     </div>
   )
