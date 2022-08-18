83d5023fba669c079dd1857d562d79faffdbd0c0
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-18 12:40:34 +0200

    Remove padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import ConnectWalletButton from './ConnectWalletButton'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center h-20 max-w-7xl mx-auto pt-8 px-8'>
+    <div className='flex justify-between items-center h-20 max-w-7xl mx-auto pt-8'>
         {/* Logo container */}
         <div className='flex items-baseline'>
             <h2 className='text-2xl sm:text-3xl  font-extrabold'>Chainboard</h2>
