d5974a4e106859b90f113aa07ff23ed0fcf769f3
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-19 15:32:45 +0200

    Add fonctions from the smart contract

--- src/contexts/BlockchainContext.jsx
+++ src/contexts/BlockchainContext.jsx
@@ -8,7 +8,15 @@ const BlockchainContext = createContext();
 
 export const BlockchainProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState("");
-    const [balance, setBalance] = useState();
+    const [renter, setRenter] = useState();
+    const [renterExists, setRenterExists] = useState();
+    const [due, setDue] = useState();
+    const [actualDuration, setActualDuration] = useState();
+    const [totalDuration, setTotalDuration] = useState();
+    const [owner, setOwner] = useState(false);
+    const [renterBalance, setRenterBalance] = useState();
+    const [contractBalance, setContractBalance] = useState();
+    const [ownerBalance, setOwnerBalance] = useState();
 
     // Read-only access to the blockchain
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
@@ -36,7 +44,7 @@ export const BlockchainProvider = ({ children }) => {
     }
 
     // Check if wallet is connected
-    const checkWalletConnection = async () => {
+    const checkWalletConnection = async() => {
         try {
             // Is Metamask installed?
             if(!window.ethereum) return alert("Please install Metamask.")
@@ -55,30 +63,251 @@ export const BlockchainProvider = ({ children }) => {
         }
     }
 
-    // Get contract's balance
-    const getContractBalance = async () => {
+    // Add the user as a renter
+    const addRenter = async(currentAccount, canRent, isRenting, balance, due, start, end) => {
         try {
-            const contractBalance = await contract.balanceOf();
-            setBalance(ethers.utils.formatEther(contractBalance));
+            console.log(`[addRenter]currentAccount: ${currentAccount} - ${canRent} - ${isRenting} - ${balance} - ${due} - ${start} - ${end}`);
+            const addRenter = await contract.addRenter(currentAccount, canRent, isRenting, balance, due, start, end);
+            await addRenter.wait();
+            checkRenterExists();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Check if the renter exists
+    const checkRenterExists = async() => {
+        try {
+            console.log('[checkRenterExists]currentAccount: ' + currentAccount);
+            if(currentAccount) {
+                // Return true if renter exists
+                const renterExists = await contract.renterExists(currentAccount);
+                setRenterExists(renterExists);
+                console.log('[checkRenterExists]renterExists: ' + renterExists);
+
+                // If renter exists, we get his status (canRent and isRenting)
+                if(renterExists) {
+                    await getRenterStatus();
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get renter's information
+    const getRenterStatus = async() => {
+        try {
+            if(currentAccount) {
+                const renter = await contract.getRenterStatus(currentAccount);
+                // Save canRent and isRenting values in the state
+                setRenter(renter);
+                console.log('[getRenterStatus]renter: ' + renter);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get renter's balance
+    const getRenterBalance = async() => {
+        try {
+            if(currentAccount) {
+                const renterBalance = await contract.balanceOfRenter(currentAccount);
+                // Store the balance in the state
+                setRenterBalance(ethers.utils.formatEther(renterBalance))
+                console.log('[getRenterBalance]renterBalance: ' + renterBalance);
+            }            
         } catch (error) {
             console.log(error);            
+        }
+    }
+
+    // Deposit BNB into the smart contract
+    const deposit = async(value) => {
+        try {
+            if(currentAccount) {
+                const bnbValue = ethers.utils.parseEther(value);
+                // Specify the value of the message
+                const deposit = await contract.deposit(currentAccount, { value: bnbValue });
+                await deposit.wait();
+                console.log('[deposit]deposit: ' + deposit);
+                await getRenterBalance();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get the renter's due
+    const getDue = async() => {
+        try {
+            if(currentAccount) {
+                // TO DO: add price as a state to send the price of the selected snowboard
+                const due = await contract.getDue(currentAccount);
+                // Store the due in the state
+                setDue(ethers.utils.formatEther(due));
+                console.log('[getDue]due: ' + due);                
+            }            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get the actual duration
+    const getActualDuration = async() => {
+        try {
+            if(currentAccount){
+                const actualDuration = await contract.getActualDuration(currentAccount);
+                // Store the actualDuration in the state and make it a number
+                setActualDuration(Number(actualDuration));
+                console.log('[getActualDuration]actualDuration: ' + actualDuration);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get the total duration
+    const getTotalDuration = async() => {
+        try {
+            if(currentAccount) {
+                const totalDuration = await contract.getTotalDuration(currentAccount);
+                // Store the totalDuration in the state and make it a number
+                setTotalDuration(Number(totalDuration));
+                console.log('[getTotalDuration]totalDuration: ' + totalDuration);
+            }            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Make the payment
+    const makePayment = async(value) => {
+        try {
+            if(currentAccount){
+                const bnbValue = ethers.utils.parseEther(value);
+                const deposit = await contract.makePayment(currentAccount, bnbValue);
+                await deposit.wait();
+
+                // Check if canRent, isRenting, balance, totalDuration and due are correctly reset and store the results in appropriate states
+                await getRenterStatus();
+                await getRenterBalance();
+                await getTotalDuration();
+                await getDue();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Call checkout function
+    const checkOut = async() => {
+        try {
+            const checkOut = await contract.checkOut(currentAccount);
+            await checkOut.wait();
+
+            // Update renter's status
+            await getRenterStatus();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Call checkin function
+    const checkIn = async() => {
+        try {
+            const checkIn = await contract.checkIn(currentAccount);
+            await checkIn.wait();
+
+            // Update renter's status, due actual duration and total duration
+            await getRenterStatus();
+            await getDue();
+            await getActualDuration();
+            await getTotalDuration();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Call isOwner function
+    const isOwner = async() => {
+        try {
+            if(currentAccount) {
+                const owner = await contract.isOwner();
+                // Store the owner (boolean) in the state
+                setOwner(owner);
+                console.log('[isOwner]owner: ' + owner);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get contract's balance
+    const getContractBalance = async() => {
+        try {
+            const contractBalance = await contract.balanceOf();
+            setContractBalance(ethers.utils.formatEther(contractBalance));
+            console.log('[getContractBalance]contractBalance: ' + contractBalance);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Get owner's balance
+    const getOwnerBalance = async() => {
+        try {
+            if(owner) {
+                const ownerBalance = await contract.balanceOfOwner();
+                setOwnerBalance(ethers.utils.formatEther(ownerBalance));
+                console.log('[getOwnerBalance]ownerBalance: ' + ownerBalance);
+            } else {
+                console.log('[getOwnerBalance]owner: ' + owner);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Owner withdraw
+    const ownerWithdraw = async() => {
+        try {
+            const ownerBalance = await contract.withdrawOwnerBalance();
+            await ownerBalance.wait;
+            // Update owner's balance and contract's balance
+            await getOwnerBalance();
+            await getContractBalance();            
+        } catch (error) {
+            console.log(error);
         }
     }
 
     // When variables changes
     useEffect(() => {
         checkWalletConnection();
+        checkRenterExists();
+        getRenterBalance();
+        getDue();
+        // getActualDuration();
+        getTotalDuration();
+        isOwner();
         getContractBalance();
-    }, [])
+        getOwnerBalance();
+    }, [currentAccount])
 
   return (
     <BlockchainContext.Provider
         value={{
             connectWallet,
-            currentAccount
+            currentAccount,
+            renterExists,
+            addRenter,
+            renter,
+            checkOut
         }}
     >
-        {children}
+        { children }
     </BlockchainContext.Provider>
   )
 }
