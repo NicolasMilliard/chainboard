159a7520e84b50f36f7c6646cad0940d5d87ea75
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-19 15:31:44 +0200

    Init commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ text, customFunc }) => {
+  return (
+    <button type='button' onClick={customFunc} className='chainboard-default-btn bg-[#f2504b] px-4 py-2 text-white text-14 sm:text-base font-semibold rounded-3xl hover:drop-shadow-lg' >
+        {text}
+    </button>
+  )
+}
+
+export default Button
