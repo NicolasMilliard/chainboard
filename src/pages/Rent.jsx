b51e040b17c8f7b50a3738b86bc7fbaa6550d682
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-20 18:38:27 +0200

    Add checkIn function

--- src/pages/Rent.jsx
+++ src/pages/Rent.jsx
@@ -4,15 +4,43 @@ import LevelSelect from '../components/LevelSelect'
 import SnowboardSize from '../components/SnowboardSize'
 import ProductCarousel from '../components/ProductCarousel'
 import BnbPrice from '../components/BnbPrice'
+import Button from '../components/Button'
 
+import { useStateBlockchainContext } from '../contexts/BlockchainContext'
+import { useStateContext } from '../contexts/ContextProvider'
+
+import { snowboards } from '../data/snowboards'
 
 const Rent = () => {
+  const { currentAccount, checkOut, checkIn, addRenter, renterExists } = useStateBlockchainContext();
+  const { level } = useStateContext();
+
+  const handleSubmit = async() => {
+    await addRenter(currentAccount, true, false, 0, 0, 0, 0);
+    console.log('[handleSubmit]: renter added');
+  }
+
+  const handleCheckIn = async() => {
+    snowboards.map(item => {
+      if(item.level === level) {
+        const price = item.price;
+        // price must be sent in wei as a string
+        const weiPrice = (price * (10 ** 18)).toString();
+        console.log('currentAccount: ' + currentAccount + ' - price: ' + weiPrice);
+        checkIn(weiPrice);
+      }
+    })
+  }
+
   return (
-    <div className='flex flex-grow flex-shrink basis-0 justify-between mx-auto max-w-7xl mt-20 sm:mt-40'>
+    <div className='flex flex-grow flex-shrink basis-0 justify-between mx-auto max-w-7xl mt-20 px-8 sm:mt-40'>
         <div className='flex flex-col flex-grow flex-shrink basis-0'>
             <LevelSelect />
             <SnowboardSize />
             <BnbPrice />
+            {/* If the user is a renter, he can checkout. If he's not, he must create his profile before renting his snowboard */}
+            {renterExists ? <Button text='Check out' customFunc={checkOut} /> : <Button text='Create my profile' customFunc={handleSubmit} />}
+            <Button text='Check in' customFunc={handleCheckIn} />
         </div>
         <div className='flex flex-grow flex-shrink basis-0'>
           <ProductCarousel />
