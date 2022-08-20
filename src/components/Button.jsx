1e3f41122ce29802db1ffdc6b0f2c1c387112110
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-20 18:38:40 +0200

    Add margin bottom

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ text, customFunc }) => {
   return (
-    <button type='button' onClick={customFunc} className='chainboard-default-btn bg-[#f2504b] px-4 py-2 text-white text-14 sm:text-base font-semibold rounded-3xl hover:drop-shadow-lg' >
+    <button type='button' onClick={customFunc} className='chainboard-default-btn bg-[#f2504b] mb-6 px-4 py-2 text-white text-14 sm:text-base font-semibold rounded-3xl hover:drop-shadow-lg' >
         {text}
     </button>
   )
