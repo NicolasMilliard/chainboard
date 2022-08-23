fe2bc4ec92ac20849e8d03e106fa2b0c67ecf7e8
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-23 16:48:40 +0200

    Add links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import ConnectWalletButton from './ConnectWalletButton'
 
+import { useStateBlockchainContext } from '../contexts/BlockchainContext'
+
 const Navbar = () => {
+  const { owner } = useStateBlockchainContext();
+
   return (
     <div className='flex justify-between items-center h-20 max-w-7xl mx-auto px-8 pt-8'>
+      <div className='flex items-center'>
         {/* Logo container */}
-        <div className='flex items-baseline'>
+        <Link to='/' className='flex items-baseline mr-8'>
             <h2 className='text-2xl sm:text-3xl  font-extrabold'>Chainboard</h2>
             <span className='chainboard-dot ml-1'></span>
-        </div>
+        </Link>
+        {/* If user is the owner of the contract */}
+        {owner && <Link to='/admin'>Admin</Link>}
+      </div>
         <ConnectWalletButton />        
     </div>
   )
