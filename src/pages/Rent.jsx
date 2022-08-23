74f8e3b041551e2b88076145167383a3b83642a9
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-23 16:50:37 +0200

    Remove checkIn and owner's functions

--- src/pages/Rent.jsx
+++ src/pages/Rent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import LevelSelect from '../components/LevelSelect'
 import SnowboardSize from '../components/SnowboardSize'
@@ -7,29 +8,24 @@ import BnbPrice from '../components/BnbPrice'
 import Button from '../components/Button'
 
 import { useStateBlockchainContext } from '../contexts/BlockchainContext'
-import { useStateContext } from '../contexts/ContextProvider'
-
-import { snowboards } from '../data/snowboards'
 
 const Rent = () => {
-  const { currentAccount, checkOut, checkIn, addRenter, renterExists, due, deposit, makePayment } = useStateBlockchainContext();
-  const { level } = useStateContext();
+  const { currentAccount, checkOut, renter, addRenter, renterExists } = useStateBlockchainContext();
+  let navigate = useNavigate();
 
   const handleSubmit = async() => {
     await addRenter(currentAccount, true, false, 0, 0, 0, 0);
     console.log('[handleSubmit]: renter added');
   }
 
-  const handleCheckIn = async() => {
-    snowboards.map(item => {
-      if(item.level === level) {
-        const price = item.price;
-        // price must be sent in wei as a string
-        const weiPrice = (price * (10 ** 18)).toString();
-        console.log('currentAccount: ' + currentAccount + ' - price: ' + weiPrice);
-        checkIn(weiPrice);
-      }
-    })
+  const handleCheckOut = async() => {
+    checkOut();
+    
+    if(renter.isRenting) {
+      navigate('/renting');
+    } else {
+      console.log(renter.isRenting);
+    }
   }
 
   return (
@@ -38,12 +34,7 @@ const Rent = () => {
             <LevelSelect />
             <SnowboardSize />
             <BnbPrice />
-            {/* If the user is a renter, he can checkout. If he's not, he must create his profile before renting his snowboard */}
-            {renterExists ? <Button text='Check out' customFunc={checkOut} /> : <Button text='Create my profile' customFunc={handleSubmit} />}
-            <Button text='Check in' customFunc={handleCheckIn} />
-            <Button text='Deposit' customFunc={() => deposit('0.2')} />
-            {/* If due greater than 0 */}
-            {due > 0 ? <Button text='Make Payment' customFunc={makePayment} /> : ''}
+            {renterExists ? <Button text='Check out' customFunc={handleCheckOut} /> : <Button text='Create my profile' customFunc={handleSubmit} />}
         </div>
         <div className='flex flex-grow flex-shrink basis-0'>
           <ProductCarousel />
