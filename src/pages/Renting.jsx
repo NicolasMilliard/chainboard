ba53674be9e14642429a076ddd57583476a27ffe
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-25 11:04:37 +0200

    Init commit

--- src/pages/Renting.jsx
+++ src/pages/Renting.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import Button from '../components/Button';
+import Button from '../components/Button'
+import BnbPrice from '../components/BnbPrice'
+import ProductCarousel from '../components/ProductCarousel'
+import ActualDuration from '../components/ActualDuration'
 
 import { useStateBlockchainContext } from '../contexts/BlockchainContext'
 import { useStateContext } from '../contexts/ContextProvider'
@@ -23,13 +26,20 @@ const Renting = () => {
       }
 
   return (
-    <div>
-        {/* If the user is currently renting a snowboard */}
-        {/* {renter.isRenting && <Button text='Check in' customFunc={handleCheckIn} />} */}
-        <Button text='Check in' customFunc={handleCheckIn} />
-        <Button text='Deposit' customFunc={() => deposit('0.02')} />
+    <div className='flex flex-grow flex-shrink basis-0 justify-between mx-auto max-w-7xl mt-20 px-8 sm:mt-40'>
+        <div className='flex flex-col flex-grow flex-shrink basis-0'>
+            <ActualDuration />
+            <BnbPrice />
+            {/* If the user is currently renting a snowboard */}
+            {/* {renter.isRenting && <Button text='Check in' customFunc={handleCheckIn} />} */}
+            <Button text='Check in' customFunc={handleCheckIn} />
+            <Button text='Deposit' customFunc={() => deposit('0.02')} />
         {/* If due greater than 0 */}
         {due > 0 && <Button text='Make Payment' customFunc={makePayment} />}
+        </div>
+        <div className='flex flex-grow flex-shrink basis-0'>
+          <ProductCarousel />
+        </div>
     </div>
   )
 }
