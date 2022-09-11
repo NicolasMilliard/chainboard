3911e3ca24aa3845321cab41abd03b1c9e143ad2
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-11 16:28:29 +0200

    Align label with input and span

--- src/components/SnowboardSize.jsx
+++ src/components/SnowboardSize.jsx
@@ -19,10 +19,10 @@ const SnowboardSize = () => {
   return (
     <div className='mb-20'>
         <h2 className='font-semibold text-xl sm:text-2xl sm:leading-tight mb-8'>Choose your snowboard size:</h2>
-        <div className='flex flex-wrap'>
-          <label htmlFor='snowboard-size' className='mr-2'>Size:</label>
+        <div className='flex flex-wrap items-center'>
+          <label htmlFor='snowboard-size' className='mr-3'>Size:</label>
           <input id='snowboard-size' type='range' min='142' max='167' className='chainboard-input-range' onChange={handleSnowboardSize} onInput={handleInputChange} />
-          <span id='snowboard-size-choice' className='ml-2'>{size} cm</span>            
+          <span id='snowboard-size-choice' className='ml-3'>{size} cm</span>            
         </div>
         <button>Calculate my size's snowboard</button>
     </div>
