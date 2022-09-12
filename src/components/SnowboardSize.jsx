c3d2dd6441c0a8a0f3f4b1d796622cc7ebcdcd49
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-12 16:08:56 +0200

    Manage the display of modal component

--- src/components/SnowboardSize.jsx
+++ src/components/SnowboardSize.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useStateContext } from '../contexts/ContextProvider';
 
 const SnowboardSize = () => {
-  const { size, handleSnowboardSize } = useStateContext();
+  const { size, handleSnowboardSize, setIsDisplay } = useStateContext();
 
   // Manage background color's track of the input range
   const handleInputChange = (e) => {
@@ -21,10 +21,10 @@ const SnowboardSize = () => {
         <h2 className='font-semibold text-xl sm:text-2xl sm:leading-tight mb-8'>Choose your snowboard size:</h2>
         <div className='flex flex-wrap items-center'>
           <label htmlFor='snowboard-size' className='mr-3'>Size:</label>
-          <input id='snowboard-size' type='range' min='142' max='167' className='chainboard-input-range' onChange={handleSnowboardSize} onInput={handleInputChange} />
+          <input id='snowboard-size' type='range' min='138' max='167' className='chainboard-input-range' onChange={handleSnowboardSize} onInput={handleInputChange} />
           <span id='snowboard-size-choice' className='ml-3'>{size} cm</span>            
         </div>
-        <button>Calculate my size's snowboard</button>
+        <button onClick={() => {setIsDisplay(true)}}>Calculate my size's snowboard</button>
     </div>
   )
 }
