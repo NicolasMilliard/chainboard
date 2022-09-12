00451aa2aa818db96d2e681e3393e3d7a36776bb
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-12 16:08:06 +0200

    Init commit

--- src/components/FlashIcon.jsx
+++ src/components/FlashIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Flash = () => {
+const FlashIcon = () => {
   return (
     <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g>
@@ -10,4 +10,4 @@ const Flash = () => {
   )
 }
 
-export default Flash
+export default FlashIcon
