3b99565c82ff8fadb4f91c67ca0932d71ab09877
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-13 11:00:18 +0200

    Rework index

--- src/components/BnbPrice.jsx
+++ src/components/BnbPrice.jsx
@@ -6,24 +6,24 @@ import { snowboards } from '../data/snowboards'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const BnbPrice = () => {
-    const { level } = useStateContext();
+  const { level } = useStateContext();
   return (
     <div className='flex flex-wrap mb-20'>
-        <div className='flex flex-wrap items-center'>
-            <img src={bsc} alt='Binance Smart Chain' className='chainboard-bsc-logo-price mr-4' />
-            <div className='chainboard-bsc-separator'></div>
-        </div>
-        <div className='ml-4'>
-            {snowboards.map(item => {
-              if(item.level === level)
-                return (
-                    <p key={item.level}>
-                        <span className='text-[#f2504b] font-semibold'>{item.price} BNB / hour</span><br />
-                        <span>{item.price * 8} BNB / day</span>
-                    </p>
-                )
-            })}
-        </div>
+      <div className='flex flex-wrap items-center'>
+        <img src={bsc} alt='Binance Smart Chain' className='chainboard-bsc-logo-price mr-4' />
+        <div className='chainboard-bsc-separator'></div>
+      </div>
+      <div className='ml-4'>
+        {snowboards.map(item => {
+          if(item.level === level)
+          return (
+            <p key={item.level}>
+              <span className='text-[#f2504b] font-semibold'>{item.price} BNB / hour</span><br />
+              <span>{item.price * 8} BNB / day</span>
+          </p>
+          )
+        })}
+      </div>
     </div>
   )
 }
