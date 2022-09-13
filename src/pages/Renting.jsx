44d5ee46f5bd3d11da48d732e4030dbb6a508e2e
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-13 11:01:44 +0200

    Replace BnbPrice by RentingPrice. Add redirection

--- src/pages/Renting.jsx
+++ src/pages/Renting.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import Button from '../components/Button'
-import BnbPrice from '../components/BnbPrice'
+import RentingPrice from '../components/RentingPrice'
 import ProductCarousel from '../components/ProductCarousel'
 import ActualDuration from '../components/ActualDuration'
 
@@ -12,6 +14,16 @@ import { snowboards } from '../data/snowboards'
 const Renting = () => {
     const { currentAccount, renter, checkIn, deposit, due, makePayment } = useStateBlockchainContext();
     const { level } = useStateContext();
+    let navigate = useNavigate();
+
+    useEffect(() => {
+      // if renting is false, redirect to the home/rent page
+      if(renter) {
+        if(renter.isRenting === false) {
+          navigate('/');
+        }
+      }
+    })
 
     const handleCheckIn = async() => {
         snowboards.map(item => {
@@ -28,14 +40,13 @@ const Renting = () => {
   return (
     <div className='flex flex-grow flex-shrink basis-0 justify-between mx-auto max-w-7xl mt-20 px-8 sm:mt-40'>
         <div className='flex flex-col flex-grow flex-shrink basis-0'>
-            <ActualDuration />
-            <BnbPrice />
-            {/* If the user is currently renting a snowboard */}
-            {/* {renter.isRenting && <Button text='Check in' customFunc={handleCheckIn} />} */}
-            <Button text='Check in' customFunc={handleCheckIn} />
-            <Button text='Deposit' customFunc={() => deposit('0.02')} />
-        {/* If due greater than 0 */}
-        {due > 0 && <Button text='Make Payment' customFunc={makePayment} />}
+          <ActualDuration />
+          <RentingPrice />
+          {/* If the user is currently renting a snowboard */}
+          { renter && renter.isRenting && <Button text='Check in' customFunc={handleCheckIn} />}
+          <Button text='Deposit' customFunc={() => deposit('0.02')} />
+          {/* If due greater than 0 */}
+          {due > 0 && <Button text='Make Payment' customFunc={makePayment} />}
         </div>
         <div className='flex flex-grow flex-shrink basis-0'>
           <ProductCarousel />
