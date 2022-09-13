96dca6712ccb431c5e30da6a63d1fe966b069846
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-13 11:00:03 +0200

    Get the start rating date

--- src/components/ActualDuration.jsx
+++ src/components/ActualDuration.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 import { useStateBlockchainContext } from '../contexts/BlockchainContext'
 
 const ActualDuration = () => {
-    // const { actualDuration } = useStateBlockchainContext();
-    const actualDuration = 1661270497000;
-    const timestamp = new Date(actualDuration);
-    const duration = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(timestamp.getTime());
-    // const duration = timestamp.toDateString();
+    const { actualDuration } = useStateBlockchainContext();
+    // IMPORTANT: for testing, the price per hour is converted to price per minute (so all calculations are relative to minutes)
+    const MS_PER_MINUTE = 60000;
+    const startRentingDate = new Date(new Date() - (actualDuration * MS_PER_MINUTE));
+    const startRentingDateFormat = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(startRentingDate.getTime());
   return (
     <div className='mb-20'>
         <p className='mb-4'>Your rental has started on:</p>
-        <p className='font-semibold text-xl'>{duration}</p>
+        <p className='font-semibold text-xl'>{startRentingDateFormat}</p>
     </div>
   )
 }
