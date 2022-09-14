f7a48d8dad77f51b99658f93c71a7ed93abfe260
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-14 13:51:02 +0200

    Update smart contract (remove deposit system)

--- contracts/Chainboard.sol
+++ contracts/Chainboard.sol
@@ -1,31 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import "./SafeMath.sol";
+
 contract Chainboard {
     address owner;
-    uint ownerBalance;
 
     constructor() {
         owner = msg.sender;
     }
 
     struct Renter {
-        address payable walletAddress;
+        address walletAddress;
         bool canRent;
         bool isRenting;
-        uint balance;
         uint due;
         uint start;
         uint end;
     }
 
     mapping (address => Renter) public renters;
-
-    // Add the renter to the blockchain
-    function addRenter(address payable walletAddress, bool canRent, bool isRenting, uint balance, uint due, uint start, uint end) public {
-        // Key is the walletAddress
-        renters[walletAddress] = Renter(walletAddress, canRent, isRenting, balance, due, start, end);
-    }
 
     modifier isRenter(address walletAddress) {
         require(msg.sender == walletAddress, "You can only manage your account.");
@@ -37,9 +31,31 @@ contract Chainboard {
         _;
     }
 
+    // Add the renter to the blockchain
+    function addRenter(address walletAddress, bool canRent, bool isRenting, uint due, uint start, uint end) public {
+        require(renterExists(walletAddress) == false, "You already have an account.");
+        // Key is the walletAddress
+        renters[walletAddress] = Renter(walletAddress, canRent, isRenting, due, start, end);
+    }
+
+    // Get renter informations
+    function getRenterStatus(address walletAddress) public isRenter(walletAddress) view returns(bool canRent, bool isRenting) {
+        canRent = renters[walletAddress].canRent;
+        isRenting = renters[walletAddress].isRenting;
+    }
+
+    // Check if renter exist
+    function renterExists(address walletAddress) public isRenter(walletAddress) view returns(bool) {
+        // address(0) is the default value for an address
+        if(renters[walletAddress].walletAddress != address(0)) {
+            return true;
+        }
+        return false;
+    }
+
     // Check out a snowboard
     function checkOut(address walletAddress) public isRenter(walletAddress) {
-        require(renters[walletAddress].due == 0, "You have a pending balance.");
+        require(renters[walletAddress].due == 0, "You have a pending payment.");
         require(renters[walletAddress].canRent == true, "You can't rent at this time.");
 
         renters[walletAddress].isRenting = true;
@@ -58,108 +74,63 @@ contract Chainboard {
         setDue(walletAddress, snowboardPrice);
     }
 
+    // Set due amount (calculated in minutes and not in hours to facilite testing phase)
+    function setDue(address walletAddress, uint snowboardPrice) internal {
+        uint timespanMinutes = getTotalDuration(walletAddress);
+        // snowboardPrice must be in wei format (16 decimals), for example 0.012 BNB = 12000000000000000 wei
+        renters[walletAddress].due = SafeMath.mul(timespanMinutes, snowboardPrice);
+    }
+
+    // Get due amount
+    function getDue(address walletAddress) public isRenter(walletAddress) view returns(uint) {
+        return renters[walletAddress].due;
+    }
+
     // Get actual duration of snowboard use
-    function getActualDuration(address walletAddress) public isRenter(walletAddress) view returns(uint) {
+    function getActualDuration(address walletAddress) public view isRenter(walletAddress) returns(uint) {
         require(renters[walletAddress].isRenting == true, "You don't have a snowboard.");
 
-        uint durationInSeconds = block.timestamp - renters[walletAddress].start;
-        // Get duration in minutes
-        uint duration = durationInSeconds / 60;
-        return duration;
+        uint durationInSeconds = SafeMath.sub(block.timestamp, renters[walletAddress].start);
+
+        return handleDuration(durationInSeconds);
     }
 
     // Get total duration of snowboard use
-    function getTotalDuration(address walletAddress) public isRenter(walletAddress) view returns(uint) {
-        if(renters[walletAddress].start == 0 || renters[walletAddress].end == 0) {
-            return 0;
+    function getTotalDuration(address walletAddress) public view isRenter(walletAddress) returns(uint) {
+        require(renters[walletAddress].isRenting == false, "You are still renting a snowboard");
+        require(renters[walletAddress].start > 0, "You are not renting a snowboard");
+        require(renters[walletAddress].end > 0, "You are still renting a snowboard");
+        
+        uint durationInSeconds = SafeMath.sub(renters[walletAddress].end, renters[walletAddress].start);
+
+        return handleDuration(durationInSeconds);
+    }
+
+    // Detect if duration is less than one minute and convert duration in minutes
+    function handleDuration(uint durationInSeconds) pure internal returns(uint) {
+        if(durationInSeconds >= 60) {
+            // Get duration in minutes
+            uint durationInMinutes = SafeMath.div(durationInSeconds, 60);
+            return durationInMinutes;
         } else {
-            uint durationInSeconds = renters[walletAddress].end - renters[walletAddress].start;
-            // Minimum duration is 1 minute
-            if(durationInSeconds >= 60) {
-                // Get duration in minutes
-                uint duration = durationInSeconds / 60;
-                return duration;
-            } else {
-                return 1;
-            }
+            return 1;
         }
     }
 
-    // Get contract's balance
-    function balanceOf() public view returns(uint) {
-        // this refers to the contract
-        return address(this).balance;
-    }
-
-    // Check if it's the owner
+    // Check if the current account is the owner
     function isOwner() public view returns(bool) {
         return owner == msg.sender;
     }
 
-    // Get owner's balance
-    function balanceOfOwner() public view onlyOwner() returns(uint) {
-        return ownerBalance;
-    }
-
-    // Withdraw to owner's balance
-    function withdrawOwnerBalance() payable public {
-        payable(owner).transfer(ownerBalance);
-        // Reset owner's balance
-        ownerBalance = 0;
-    }
-
-    // Get renter's balance
-    function balanceOfRenter(address walletAddress) public isRenter(walletAddress) view returns(uint) {
-        return renters[walletAddress].balance;
-    }
-
-    // Get renter informations
-    function getRenterStatus(address walletAddress) public isRenter(walletAddress) view returns(bool canRent, bool isRenting) {
-        canRent = renters[walletAddress].canRent;
-        isRenting = renters[walletAddress].isRenting;
-    }
-
-    // Check if renter exist
-    function renterExists(address walletAddress) public isRenter(walletAddress) view returns(bool) {
-        // address(0) is the default value for an address
-        if(renters[walletAddress].walletAddress != address(0)) {
-            return true;
-        }
-        return false;
-    }
-
-    // Set due amount (calculated in minutes and not in hours to facilite testing phase)
-    function setDue(address walletAddress, uint snowboardPrice) internal {
-        uint timespanMinutes = getTotalDuration(walletAddress);
-        // snowboardPrice must be in wei format (16 decimals), e.g. 0.012 BNB = 12000000000000000 wei
-        renters[walletAddress].due = timespanMinutes * snowboardPrice;
-    }
-
-    // Get due amount
-    function getDue(address walletAddress) public view returns(uint) {
-        return renters[walletAddress].due;
-    }
-
-    // Return a bool to check is renter can rent
-    function canRentSnowboard(address walletAddress) public isRenter(walletAddress) view returns(bool) {
-        return renters[walletAddress].canRent;
-    }
-
-    // Deposit into the smart contract (renter's balance)
-    function deposit(address walletAddress) public isRenter(walletAddress) payable {
-        renters[walletAddress].balance += msg.value;
-    }
-
-    // Make the payment and reset rents parameters
-    function makePayment(address walletAddress, uint amount) public isRenter(walletAddress) {
+    // Make the payment and reset renter parameters
+    function makePayment(address walletAddress) public isRenter(walletAddress) payable {
         require(renters[walletAddress].due > 0, "You don't have anything due at this time.");
-        require(renters[walletAddress].balance > amount, "You don't have enough funds to cover payment. Please make a deposit first.");
+        require(renters[walletAddress].due == msg.value, "You don't set the correct amount (must be equal to your due)");
 
-        // Transfer amount from renter's smart contract balance to owner's smart contract balance
-        renters[walletAddress].balance -= amount;
-        ownerBalance += amount;
+        // Transfer amount from renter's account to owner's account
+        payable(owner).transfer(msg.value);
 
-        // Balance is paid, renter can rent again
+        // Payment is made, renter can rent again
         renters[walletAddress].canRent = true;
         renters[walletAddress].due = 0;
         renters[walletAddress].start = 0;
