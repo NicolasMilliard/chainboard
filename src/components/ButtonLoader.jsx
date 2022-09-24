a34ce8282fa1e175c87ce585642cc6d0cdace9cc
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-09-24 17:06:41 +0200

    Init commit

---
+++ src/components/ButtonLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonLoader = () => {
+  return (
+    <span className='flex items-center'>
+        <span className='spin-loader'></span>Loading...
+    </span>
+  )
+}
+
+export default ButtonLoader
