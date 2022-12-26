aa857f59c74330c2dad9cd6e5a157d24a822f4ed
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-26 16:02:04 +0100

    Update router basename

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
-import Home from './pages/Home'
-import Rent from './pages/Rent'
-import Renting from './pages/Renting'
-import Payment from './pages/Payment'
+import Home from "./pages/Home";
+import Rent from "./pages/Rent";
+import Renting from "./pages/Renting";
+import Payment from "./pages/Payment";
 
-import { useStateBlockchainContext } from './contexts/BlockchainContext'
+import { useStateBlockchainContext } from "./contexts/BlockchainContext";
 
-import './App.css'
-import 'react-toastify/dist/ReactToastify.css'
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-    const { renterExists } = useStateBlockchainContext();
+  const { renterExists } = useStateBlockchainContext();
   return (
     <>
-      <BrowserRouter basename='/chainboard'>
-        <Navbar />        
+      <BrowserRouter basename="/">
+        <Navbar />
         {/* Routes */}
         <Routes>
           {/* Display snowboards selection if connected. If not, display homepage. */}
-          <Route path='/' element={renterExists ? <Rent /> : <Home />} />
-          <Route path='/renting' element={<Renting />} />
-          <Route path='/payment' element={<Payment />} />
+          <Route path="/" element={renterExists ? <Rent /> : <Home />} />
+          <Route path="/renting" element={<Renting />} />
+          <Route path="/payment" element={<Payment />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
