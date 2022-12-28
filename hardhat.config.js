bea31a4a2fc9491c61527ce1f5c65d58d170634d
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-28 14:25:44 +0100

    Update solidity version to 0.8.17

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  solidity: "0.8.17",
   networks: {
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
       accounts: [process.env.ACCOUNTS],
-    }
-  }
+    },
+  },
 };
